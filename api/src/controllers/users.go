36bef1152b060b59efbcb44f8986ef654c35804b
wagner-tanaka <wbtanaka@hotmail.com>
2023-06-16 08:02:06 +0900

    Responses

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -4,10 +4,9 @@ import (
 	"api/src/database"
 	"api/src/models"
 	"api/src/repositories"
+	"api/src/responses"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
@@ -15,33 +14,35 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	// request body
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 
 	// take the body and convert to a user struct
 	var user models.User
 	if err = json.Unmarshal(requestBody, &user); err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusBadRequest, err)
+		return
 	}
 
 	// connect to database
 	db, err := database.Connect()
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
+	defer db.Close()
 
-	// defer the database connection close
 	userRepository := repositories.NewUsersRepository(db)
 
 	// create user, passing the user from request
-	userID, err := userRepository.Create(user)
+	user.ID, err = userRepository.Create(user)
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
 
-	// return the user ID
-	w.Write([]byte(fmt.Sprintf("User created with ID %d", userID)))
-
+	responses.JSON(w, http.StatusCreated, user)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
