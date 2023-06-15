a77cc4d80b076aa80be3961d3fb09b3f0e4b4106
wagner-tanaka <wbtanaka@hotmail.com>
2023-06-15 16:02:31 +0900

    routes files

---
+++ api/src/router/routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Route struct {
+	URI         string
+	Method      string
+	Function    func(http.ResponseWriter, *http.Request)
+	RequireAuth bool
+}
+
+func Config(r *mux.Router) *mux.Router {
+	routes := usersRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
+}
